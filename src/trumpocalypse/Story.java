ba556d65d8f1d86fa2d0b3a2c94a0e0423ba8169
him <him@him-PC>
2016-11-16 10:36:38 -0600

    Added sub-story to first selection scene

--- src/trumpocalypse/Story.java
+++ src/trumpocalypse/Story.java
@@ -31,7 +31,55 @@ class Story {
                     String choice4 = "Check Inventory";
                     gc.getJFX().updateChoices(choice1, choice2, choice3, choice4);
                     progress++;
+                        switch (choice) {
+                            case 1: 
+                                gc.getJFX().updateDialog("You continue walking along Interstate 10. After a few hours of walking, are are now outside the "
+                                        + "city limits of New Orleans, LA. As you enter the city, you are approached by a stranger carrying a backpack. You're "
+                                        + "not exactly sure what he wants.");
+                                gc.getJFX().getChoicePane().getChildren();
+                                String choice1a = "Try to Walk Past the Stranger";
+                                String choice2a = "Approach the Stranger";
+                                String choice3a = "Attack the Stranger";
+                                String choice4a = "Check Inventory";
+                                gc.getJFX().updateChoices(choice1a, choice2a, choice3a, choice4a);
+                                progress++;
+                                break;
+                            case 2:
+                                gc.getJFX().updateDialog("'Hello ther stranger. The name's Fred. What can I do for you?'");
+                                gc.getJFX().getChoicePane().getChildren();
+                                String choice5a = "'I'm trying to get to Arizona.'";
+                                String choice6a = "'What's with the outfit? Halloween's over.'";
+                                String choice7a = "'You've got a lot of stuff in that cart, I want it.'";
+                                String choice8a = "Check Inventory";
+                                gc.getJFX().updateChoices(choice5a, choice6a, choice7a, choice8a);
+                                progress++;
+                                break;
+                            case 3:
+                                gc.getJFX().updateDialog("You attempt to search the cart, but are abruptly stopped by the stranger. 'Whoa there fella! "
+                                        + "What are you doing going through my stuff?'");
+                                gc.getJFX().getChoicePane().getChildren();
+                                String choice9a = "'I'm sorry, I didn't know this was yours.'";
+                                String choice10a = "'Oh relax! This stuff is junk anyway.'";
+                                String choice11a = "'Stay out of my way, or be sorry!'";
+                                String choice12a = "Check Inventory";
+                                gc.getJFX().updateChoices(choice9a, choice10a, choice11a, choice12a);
+                                progress++;
+                                break;
+                            case 4: 
+                                gc.getJFX().updateDialog("Inventory");
+                                gc.getJFX().getChoicePane().getChildren();
+                                String choice13a = "Display Weapons";
+                                String choice14a = "Display Med-Kits";
+                                String choice15a = "Display Currency";
+                                String choice16a = "Go Back";
+                                gc.getJFX().updateChoices(choice13a, choice14a, choice15a, choice16a);
+                                break;  
+                            default:
+                                gc.getJFX().updateDialog("Error");
+                                break;
+                        }
                     break;
+                    
                 case 2:
                     gc.getJFX().updateDialog("There are no useful items within the truck. You attempt to start the truck but it will not.");
                     gc.getJFX().getChoicePane().getChildren();
@@ -53,8 +101,13 @@ class Story {
                     progress++;
                     break;
                 case 4: 
-                    gc.getJFX().updateDialog("Suddenly an angry millenial clinton supporter jumps out from the dark, and kills you for having your own opinion ");
-                    progress++;
+                    gc.getJFX().updateDialog("Inventory");
+                    gc.getJFX().getChoicePane().getChildren();
+                    String choice13 = "Display Weapons";
+                    String choice14 = "Display Med-Kits";
+                    String choice15 = "Display Currency";
+                    String choice16 = "Go Back";
+                    gc.getJFX().updateChoices(choice13, choice14, choice15, choice16);
                     break;  
                 default:
                     gc.getJFX().updateDialog("Error");
