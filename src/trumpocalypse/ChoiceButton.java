d4c73960717b7a12c5ad5688cccabb218dfa532e
jmloftis87 <jml87@students.uwf.edu>
2016-11-18 00:28:31 -0600

    Added choice numbers 5 (restart) & 6 (exit). Normal choices are 1-4 story choices. If the choice id is 5 then the button is restart, if it is 6 then the button is a exit button.

--- src/trumpocalypse/ChoiceButton.java
+++ src/trumpocalypse/ChoiceButton.java
@@ -1,8 +1,10 @@
 package trumpocalypse;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  *
@@ -11,24 +13,60 @@ import javafx.scene.control.Button;
 
 public class ChoiceButton extends Button {
     public ChoiceButton(GameController gc, int choice, String text) {
+        // Used for choices in game (choice = (1-4))
+        // If choices are 5 or 6, they are used to restart and exit game
+        
         this.setText(text);
         this.setPrefSize(400, 30);
         this.setLayoutX(50);
         
-        if (choice == 1) { this.setLayoutY(10); }
-        else if (choice == 2) { this.setLayoutY(50); }
-        else if (choice == 3) { this.setLayoutY(90); }
-        else if (choice == 4) { this.setLayoutY(130); }
-        else { System.out.println("Can't position"); }
+        if (choice == 1) { 
+            this.setLayoutY(10); 
+        }
+        else if (choice == 2) { 
+            this.setLayoutY(50); 
+        }
+        else if (choice == 3) { 
+            this.setLayoutY(90); 
+        }
+        else if (choice == 4) { 
+            this.setLayoutY(130); 
+        }
+        else if (choice == 5) { // Restart Button
+            this.setLayoutY(10);
+        }
+        else if (choice == 6) { // Exit Button
+            this.setLayoutY(50);
+        }
+        else { 
+            System.out.println("Can't position"); 
+        }
         
-        this.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                gc.getStory().updateStory(choice);
-            }
-            
-        });
+        if (choice >= 1 && choice <= 4) {
+            this.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    gc.getStory().updateStory(choice);
+                }
+
+            });
+        }
+        else if (choice == 5) {
+            // Restart Button
+            this.setOnAction( __ ->
+            {
+              gc.getJFX().getStage().close();
+              Platform.runLater( () -> new Trumpocalypse().start( new Stage() ) );
+            } );
+        }
+        else if (choice == 6) {
+            // Close Button
+            this.setOnAction( __ -> {  System.exit(0); } );
+        }
+        else {
+            System.out.println("Unsupported choice range. - ChoiceButton.java");
+        }
     }
 
 }
